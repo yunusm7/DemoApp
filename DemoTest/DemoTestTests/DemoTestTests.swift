d69ea9307d96d388ccc783916fdc2b741f89771c
Mohammad Yunus <mohammad@virtualemployee.com>
2018-08-09 14:46:56 +0530

    Unit test
    newsFeed SUT added.

--- DemoTest/DemoTestTests/DemoTestTests.swift
+++ DemoTest/DemoTestTests/DemoTestTests.swift
@@ -12,20 +12,22 @@ import XCTest
 class DemoTestTests: XCTestCase {
     
     var newsFeedFailableSUT:NewsFeed!
-
+    var newsFeedSUT:NewsFeed!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         self.newsFeedFailableSUT = NewsFeed(data: self.loadDummyNewsFeedFailableData())
-
+        self.newsFeedSUT = NewsFeed(data: self.loadDummyNewsFeedData())
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        
+        self.newsFeedSUT = nil
         self.newsFeedFailableSUT = nil
-
     }
     
     func testExample() {
@@ -39,6 +41,48 @@ class DemoTestTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    //MARK: Synchronus Test
+    func testNewsFeedFailableInitializer() {
+        
+        XCTAssertNotNil(self.newsFeedSUT, "Failed to create object.")
+        
+        //XCTAssertNotNil(self.newsFeedFailableSUT, "Failed to create object.")
+    }
+    
+    
+    
+    //MARKL: Asynchronous Test
+    func testNewsFeedListing() {
+        
+        /// Get expectation object
+        let expect = expectation(description: "Success")
+        
+        let aboutNewsFeedUrl = APIPaths.baseUrl
+        
+        ServerHandler.sendGetRequest(functionName: aboutNewsFeedUrl, showLoader: true) { (result, error) in
+            //Success result from the server
+            if error == nil {
+                
+                let response = self.convertToDictionary(text: result as! String)
+                
+                let status = response!["status"] as? String ?? ""
+                if status == "OK" {
+                    XCTAssertEqual(status, "OK")
+                    expect.fulfill()
+                }else {
+                    XCTFail()
+                }
+                
+            } else {
+                print(error ?? "")
+                XCTAssertNil(error, "Unexpected error occured: \(String(describing: error?.localizedDescription))")
+            }
+        }
+        ///We have to update timeout value corresponding to request time out
+        waitForExpectations(timeout: 50) { (error) in
+            XCTAssertNil(error, "Test timed out. \(error!.localizedDescription)")
+        }
+    }
     
     //MARK: Helper Static Data
     func loadDummyNewsFeedFailableData() -> [String : Any] {
@@ -46,6 +90,26 @@ class DemoTestTests: XCTestCase {
                 "byline" : "By NICHOLAS FANDOS and KEVIN ROOSE",
                 "published_date" : "2018-07-31"
         ]
+    }
+    
+    func loadDummyNewsFeedData() -> [String : Any] {
+        return ["title" : "Facebook Identifies an Active Political Influence Campaign Using Fake Accounts",
+                "byline" : "By NICHOLAS FANDOS and KEVIN ROOSE",
+                "published_date" : "2018-07-31"
+        ]
+    }
+    
+    
+    
+    func convertToDictionary(text: String) -> [String: Any]? {
+        if let data = text.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
     }
     
 }
