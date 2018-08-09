0152ded1583ad6c058762011955e61f402dc9cba
Mohammad Yunus <mohammad@virtualemployee.com>
2018-08-09 13:42:29 +0530

    Storyboard changes

---
+++ DemoTest/DemoTest/Views/CollectionView Cell/AboutNewsFeedCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AboutNewsFeedCollectionViewCell.swift
+//  DemoTest
+//
+//  Created by Vivek kumar on 8/9/18.
+//  Copyright © 2018 Mohammad Yunus. All rights reserved.
+//
+
+import UIKit
+
+class AboutNewsFeedCollectionViewCell: UICollectionViewCell {
+    
+}
