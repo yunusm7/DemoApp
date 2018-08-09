c0867357ce09bda76b20bb0ca87744e5705e10e2
Mohammad Yunus <mohammad@virtualemployee.com>
2018-08-09 13:26:45 +0530

    Project added.

---
+++ DemoTest/DemoTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoTest
+//
+//  Created by Vivek kumar on 8/9/18.
+//  Copyright © 2018 Mohammad Yunus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
